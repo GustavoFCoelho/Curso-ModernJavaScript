da39bafb6c55d8f27d004007bbb4bc48cc1c5aa7
GustavoFCoelho <gustavo.f.coelho09@gmail.com>
2019-12-16 13:37:30 -0300

    GithubFinder Project

---
+++ Curso-ModernJavaScript-GitHubFinder/app.js
@@ -0,0 +1,18 @@
+const searchUser = document.getElementById('searchUser');
+const github = new GitHub();
+const ui = new UI();
+searchUser.addEventListener("keyup", (e) =>{
+    const userText = e.target.value;
+    if(userText !== ''){
+        github.getUser(userText).then(data => {
+            if(data.profile.message === 'Not Found'){
+                ui.showAlert('User not Found', 'alert alert-danger');
+            } else {
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+    } else {
+        ui.clearProfile();
+    }
+})
